edb411afda6d829f15186bcaaccb91b142e997e5
KlinglerG1 <106995187+KlinglerG1@users.noreply.github.com>
2025-05-11 16:51:17 -0600

    Update textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -15,17 +15,14 @@ class TextNode:
         self.text_type = text_type
         self.url = url
 
-    def __repr__(self):
-        return f"TextNode(text={self.text!r}, text_type={self.text_type}, url={self.url!r})"
-
     def __eq__(self, other):
         if not isinstance(other, TextNode):
             return False
         return (
-            self.text == other.text and
-            self.text_type == other.text_type and
-            self.url == other.url
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
         )
 
     def __repr__(self):
-        return f"TextNode({self.text!r}, {self.text_type}, {self.url!r})"
+        return f"TextNode(text={self.text!r}, text_type={self.text_type}, url={self.url!r})"

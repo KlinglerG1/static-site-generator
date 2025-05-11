c7a7fb0de10852d5416b98d3145ffbe92ac0182b
KlinglerG1 <106995187+KlinglerG1@users.noreply.github.com>
2025-05-11 16:42:33 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+# main.py
+from src.textnode import TextNode, TextType
+
+#print("hello world")
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
